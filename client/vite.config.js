c440fc9edf90e7f397e7ffcb719d18bf2a5d843e
Kevin Lindholm <kevin.a.lindholm@gmail.com>
2023-03-09 17:55:27 +0200

    fix error where require() was used by vite fixed by vite.config changes

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,11 +1,40 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
+import { NodeGlobalsPolyfillPlugin } from "@esbuild-plugins/node-globals-polyfill";
+import nodePolyfills from "rollup-plugin-polyfill-node";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: [{ find: "@", replacement: path.resolve(__dirname, "src") }],
+  },
+  base: "./",
+  // Node.js global to browser globalThis
+  define: {
+    global: "globalThis",
+  },
+  build: {
+    rollupOptions: {
+      plugins: [nodePolyfills()],
+    },
+    commonjsOptions: {
+      transformMixedEsModules: true,
+    },
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      // Node.js global to browser globalThis
+      define: {
+        global: "globalThis",
+      },
+      // Enable esbuild polyfill plugins
+      plugins: [
+        NodeGlobalsPolyfillPlugin({
+          buffer: true,
+        }),
+      ],
+    },
   },
 });
